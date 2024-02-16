34cfb0227fa5b06f2e97ad994584f56d8233a9bf
Aymen Ghazali <ayghazal@c3r6p10.1337.ma>
2024-02-16 22:30:31 +0100

    Navbar should be responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-import { logo, lock } from "../assets";
+import React, { useState } from "react";
+import { logo, lock, hamburgerMenu, close } from "../assets";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
+
   return (
     <>
       <div className="w-full h-[80px] border-b">
-        <div className="max-w-[1480px] w-full h-full m-auto flex justify-between items-center border border-black">
+        <div className="md:max-w-[1480px] max-w-[600px] w-full h-full m-auto flex justify-between items-center border border-black">
           <img src={logo} alt="logo image" className="h-[25px]" />
           <div className="">
             <ul className="hidden md:flex gap-4">
@@ -16,7 +22,7 @@ const Navbar = () => {
               <li>Pricing</li>
             </ul>
           </div>
-          <div className=" flex ">
+          <div className="hidden md:flex ">
             <button className="flex items-center bg-transparent gap-2 px-6">
               <img src={lock} alt="lock image" />
               <span className="text-gray-600">Login</span>
@@ -25,7 +31,14 @@ const Navbar = () => {
               Sign Up For Free
             </button>
           </div>
+
+          <div className="md:hidden" onClick={handleClick}>
+            <img src={toggle ? close : hamburgerMenu} alt="menu logo" />
+          </div>
+        
+        
         </div>
+        
       </div>
     </>
   );
