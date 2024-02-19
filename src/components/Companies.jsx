b538e0dd41b06c7ac1529c822986d8c32bc790db
Aymen Ghazali <aymenghazali1999@gmail.com>
2024-02-19 10:58:44 +0100

    Companies section done

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,10 +1,29 @@
 import React from "react";
-
+import {
+  companyLogo1,
+  companyLogo2,
+  companyLogo3,
+  companyLogo4,
+} from "../assets";
 const Companies = () => {
   return (
     <>
-      <div className="w-full bg-white py-24">
-        <div className="max-w-[1480px] m-auto grid md:grid-cols-2"></div>
+      <div className="w-ful bg-white py-24">
+        <div className="max-w-[950px] m-auto ">
+          <h1 className="text-[#536E96] text-center md:text-3xl text-2xl leading-10 font-semibold font-inter">
+            Trusted by over 25,000 teams around the world.
+          </h1>
+          <p className="text-[#536E96] text-center text-xl py-4 word-spacing-4">
+            Leading companies use the same courses to help employees keep their
+            skills fresh.
+          </p>
+          <div className="flex md:flex-cols justify-center py-8 md:gap-2">
+            <img src={companyLogo1} alt="companyLogo 1" />
+            <img src={companyLogo2} alt="companyLogo 2" />
+            <img src={companyLogo3} alt="companyLogo 3" />
+            <img src={companyLogo4} alt="companyLogo 4" />
+          </div>
+        </div>
       </div>
     </>
   );
